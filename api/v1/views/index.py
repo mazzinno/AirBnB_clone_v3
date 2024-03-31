6eb62f2c07341f66ec41c74de94f360f2a358715
mazzinno <abdennasr3@gmail.com>
2024-03-31 23:05:22 +0000

    adding task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 """ Main route """
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+           "amenities": Amenity,
+           "cities": City,
+           "places": Place,
+           "reviews": Review,
+           "states": State,
+           "users": User,
+           }
 
 
 @app_views.route('/status')
@@ -10,3 +28,14 @@ def status_check():
     checks status of JSON
     '''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def retrieve_number():
+    '''
+    retrieves number of objects by type
+    '''
+    obj_dict = {}
+    for key, value in classes.items():
+        obj_dict[key] = storage.count(value)
+    return jsonify(obj_dict)
