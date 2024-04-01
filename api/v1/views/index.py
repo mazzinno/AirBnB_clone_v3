22400e6ee9e6eb9b67c6d917ddf5872a3c9389da
Oycctem <ousstemm@gmail.com>
2024-04-01 14:35:18 +0000

    states

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-""" Main route """
-from models import storage
-from flask import jsonify
+"""Define routes for blueprint
+"""
+
 from api.v1.views import app_views
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status')
-def status_check():
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """Return status of application
     """
-    checks status of JSON
-    """
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """Retrieve count of objects in storage for various classes
+    """Retrieve count of objects in storage
     """
-    # Define classes and their corresponding names
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
     classes = {"amenities": Amenity, "cities": City,
                "places": Place, "reviews": Review,
                "states": State, "users": User}
