96861729ab59380f0daa3a139f24918f0053363c
Oycctem <ousstemm@gmail.com>
2024-04-01 12:28:20 +0000

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -39,3 +39,26 @@ def retrieve_number():
     for key, value in classes.items():
         obj_dict[key] = storage.count(value)
     return jsonify(obj_dict)
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Retrieve count of objects in storage for various classes
+    """
+    # Import necessary models
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    # Define classes and their corresponding names
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    json_dict = {}
+
+    for name, cls in classes.items():
+        json_dict.update({name: storage.count(cls)})
+
+    return jsonify(json_dict)
