a4d5902a29bda2db76c1b36798464dfa4028f94c
Oycctem <ousstemm@gmail.com>
2024-04-02 01:02:39 +0000

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-''' Runs the web app '''
-import os
-from flask import Flask, jsonify
-from models import storage
+""" Runs the web app """
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {'message': 'This is sample data from the API'}
     return jsonify(data)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
