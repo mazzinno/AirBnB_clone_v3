15d8a6dc9c389fec8b186dd9ddfb753047f402e2
Oycctem <ousstemm@gmail.com>
2024-04-02 12:34:41 +0000

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-""" Runs the web app """
-from api.v1.views import app_views
+''' Runs the web app '''
+import os
 from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
+from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'This is sample data from the API'}
-    return jsonify(data)
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''
+    teardown app context
+    '''
+    storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error': 'Not found'}), 404
+def not_found_error(error):
+    '''
+    teardown app context
+    '''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=os.getenv('HBNB_API_PORT', '5000'), threaded=True)
